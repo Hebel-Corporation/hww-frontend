3710cf133ead868cbb81ada0ca3394b9b6797b2f
Nelson Kayisirirya <nelson.kayisirirya@quantumvertex.cd>
2024-08-15 14:52:51 +0100

    Add middleware file and logic to the project

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { updateSession } from "./utils/server-auth-utils";
+import { hasAuthorization } from "./utils/client-utils";
+
+
+export async function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+    const skipAuthPattern  = /^\/compte(?:\/[\w-]+)*\/profil\/set-password$/.test(pathname);
+
+    if (skipAuthPattern) {
+        return NextResponse.next();
+    }
+
+    const response = await updateSession(request);
+
+    if (!response) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+
+    try {
+        // Define the paths that require admin access
+        const adminPaths = ['/collaborateurs', '/rapports'];
+
+        if (
+            adminPaths.some(path => request.nextUrl.pathname.startsWith(path)) && 
+            !hasAuthorization(['Administrateur'], response.cookies.get('session')?.value)) {
+            return NextResponse.redirect(new URL('/unauthorized', request.url));
+        }
+
+        return response;
+    } catch (err) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+}
+
+
+export const config = {
+    matcher: [
+        '/accueil',
+        '/collaborateurs',
+        '/rapports',
+        '/dossiers',
+        '/dossiers/:path*',
+        '/compte/:path*',
+    ],
+};
+
+
