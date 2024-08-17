c1061fa70f45887c8ef5e5dd12443b60a33a0f4f
Nelson Kayisi <nelsonkayisirirya5@gmail.com>
2024-08-17 19:51:39 +0100

    Add recure paths in the middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,46 +4,48 @@ import { hasAuthorization } from "./utils/client-utils";
 
 
 export async function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl;
-    const skipAuthPattern  = /^\/compte(?:\/[\w-]+)*\/profil\/set-password$/.test(pathname);
+    return NextResponse.next();
+    // const { pathname } = request.nextUrl;
+    // const skipAuthPattern  = /^\/compte(?:\/[\w-]+)*\/profil\/set-password$/.test(pathname);
 
-    if (skipAuthPattern) {
-        return NextResponse.next();
-    }
+    // if (skipAuthPattern) {
+    //     return NextResponse.next();
+    // }
 
-    const response = await updateSession(request);
+    // const response = await updateSession(request);
 
-    if (!response) {
-        return NextResponse.redirect(new URL('/', request.url));
-    }
+    // if (!response) {
+    //     return NextResponse.redirect(new URL('/', request.url));
+    // }
 
 
-    try {
-        // Define the paths that require admin access
-        const adminPaths = ['/collaborateurs', '/rapports'];
+    // try {
+    //     // Define the paths that require admin access
+    //     const adminPaths = ['/point-of-sale', '/rewards', 'locations'];
 
-        if (
-            adminPaths.some(path => request.nextUrl.pathname.startsWith(path)) && 
-            !hasAuthorization(['Administrateur'], response.cookies.get('session')?.value)) {
-            return NextResponse.redirect(new URL('/unauthorized', request.url));
-        }
+    //     if (
+    //         adminPaths.some(path => request.nextUrl.pathname.startsWith(path)) && 
+    //         !hasAuthorization(['Administrateur'], response.cookies.get('session')?.value)) {
+    //         return NextResponse.redirect(new URL('/unauthorized', request.url));
+    //     }
 
-        return response;
-    } catch (err) {
-        return NextResponse.redirect(new URL('/', request.url));
-    }
+    //     return response;
+    // } catch (err) {
+    //     return NextResponse.redirect(new URL('/', request.url));
+    // }
 
 }
 
 
 export const config = {
     matcher: [
-        '/accueil',
-        '/collaborateurs',
-        '/rapports',
-        '/dossiers',
-        '/dossiers/:path*',
-        '/compte/:path*',
+        '/dashboard',
+        '/locations',
+        '/account',
+        '/point-of-sale:path*',
+        '/rewards',
+        '/members/:path*',
+        '/settings',
     ],
 };
 
