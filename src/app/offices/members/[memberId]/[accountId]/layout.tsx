fcb0039926bd80624c6b29f14552b62e8f5094cf
Nelson Kayisi <nelsonkayisirirya5@gmail.com>
2024-11-14 23:32:03 +0100

    Add register payment modal

--- src/app/offices/members/[memberId]/[accountId]/layout.tsx
+++ src/app/offices/members/[memberId]/[accountId]/layout.tsx
@@ -3,6 +3,7 @@ import AccountSubMenu from '@/components/account-sub-menu'
 import { ContentLayout } from '@/components/admin-panel/content-layout'
 import SearchBar from '@/components/common/search-bar'
 import CustomBreadcrumb from '@/components/custom-breadcrumb'
+import PaymentModal from '@/components/modals/payment-modal'
 import { Chip } from '@nextui-org/react'
 import { notFound } from 'next/navigation'
 import { ReactNode } from 'react'
@@ -79,8 +80,9 @@ export default async function AccountLayout ({ children, params }: AccountLayout
         <div className='flex flex-wrap gap-4 justify-between'>
           <AccountSubMenu accountId={accountId} memberId={memberId} />
 
-          <div className="sm:w-2/4 w-full flex items-center">
+          <div className="sm:w-2/4 w-full flex gap-5 items-center">
             <SearchBar />
+            <PaymentModal />
           </div>
         </div>
 
